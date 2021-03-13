3959744449f450f12241be4f4d5c56cad752b4f6
Raj298 <71762923+Raj298@users.noreply.github.com>
2021-03-13 17:55:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video="";
 status=""
+objects=[]
 function preload(){
     video=createVideo('video.mp4');
     
@@ -15,6 +16,20 @@ video.hide();
 
 function draw(){
 image(video,0,0,480,380);
+if(status != ""){
+    objectDetector.detect(video,gotResult);
+    for(i=0; i<objects.length;i++ ){
+        document.getElementById("status").innerHTML="Satus : Object Detected";
+        document.getElementById("objects_detected").innerHTML="Number of objects detected :"+objects.length;
+
+        fill('#fc031c');
+        percent= floor(objects[i].confidence*100);
+        text(objects[i].label + " " + percent + "%", objects[i].x +15, objects[i].y+15);
+        noFill();
+        stroke('#fc031c')
+        rect(objects[i].x,objects[i].y,objects[i].width, objects[i].height);
+    }
+}
 }
 function begin(){
     objectDetector=ml5.objectDetector('cocossd',modelLoaded);
@@ -26,4 +41,15 @@ status=true
 video.loop()
 video.speed(1)
 video.volume(0)
+}
+function gotResult(error,result){
+    if(error){
+        console.log(error);
+
+    }
+    else{
+        console.log(result);
+        objects=result;
+    }
+    
 }
